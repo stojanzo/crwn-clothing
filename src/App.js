d55fb7b2831e3750737035e65d6993f6d7c135c2
unknown <zoran.stojanovic@swissonline.ch>
2020-04-19 14:55:53 +0200

    broken our homepage into smaller components, finished styling..

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
-
-import HomePage from './homepage.component';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='App'>
-        <HomePage />
-      </div>
-    );
-  }
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
